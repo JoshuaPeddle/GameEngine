d0b07111a51cb78e1aa584e3cc784615681e089e
Josh <Josh@DESKTOP-NTP2GFU>
2024-11-13 21:30:40 -0330

    Remove unused code

--- GameEngine.Core/Scene.cs
+++ GameEngine.Core/Scene.cs
@@ -4,18 +4,6 @@ namespace GameEngine.Core
 {
     public abstract class Scene
     {
-        public Dictionary<Keys, string> ActionMap = [];
-
-        public void AddAction(Keys key, string action)
-        {
-            ActionMap.Add(key, action);
-        }
-
-        public void RemoveAction(Keys key)
-        {
-            ActionMap.Remove(key);
-        }
-
         public abstract void HandleAction(Keys key, bool start);
 
         public abstract void Simulate(float deltaMs);
