3ef83d64e626644d12c42326967c9e0a9e2095b7
Josh <Josh@DESKTOP-NTP2GFU>
2025-01-18 21:18:30 -0330

    New Android runner powered by Avalonia
    Can load assets using new Asset filefetcher

---
+++ GameEngine.Demo/SceneEmpty.cs
@@ -0,0 +1,13 @@
+﻿using GameEngine.Core;
+using GameEngine.Core.Systems;
+using System;
+
+namespace GameEngine.Demo
+{
+    public class SceneEmpty : Scene
+    {
+        public SceneEmpty() { }
+
+        public override void Initialize(EntityManager entityManager, InputManager inputManager, AudioSystem audioPlayer, Action ResetScene) { }
+    }
+}
---
+++ GameEngine.Runner.Avalonia/GameEngine.Runner.Avalonia/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GameEngine.Runner.Avalonia.ViewModels
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string _greeting = "Welcome to Avalonia!";
+    }
+}
