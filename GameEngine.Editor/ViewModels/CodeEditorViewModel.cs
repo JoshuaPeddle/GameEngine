959fca53458bbc7bcea0fadd0b19554e266bbefb
Josh <Josh@DESKTOP-NTP2GFU>
2025-09-22 20:51:41 -0230

    Add SaveCommand

--- GameEngine.Editor/ViewModels/CodeEditorViewModel.cs
+++ GameEngine.Editor/ViewModels/CodeEditorViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ReactiveUI;
 using System;
 using System.IO;
+using System.Reactive;
 using System.Threading.Tasks;
 
 namespace GameEngine.Editor.ViewModels
@@ -14,12 +15,38 @@ namespace GameEngine.Editor.ViewModels
         public string SourceCode
         {
             get => _sourceCode;
-            set => this.RaiseAndSetIfChanged(ref _sourceCode, value);
+            set
+            {
+                var s = this.RaiseAndSetIfChanged(ref _sourceCode, value);
+                if (!string.IsNullOrWhiteSpace(s))
+                {
+                    if (_initialized) IsDirty = true;
+                }
+            }
         }
+
+        private bool _isDirty;
+        public bool IsDirty
+        {
+            get => _isDirty;
+            private set => this.RaiseAndSetIfChanged(ref _isDirty, value);
+        }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            private set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+        }
+
+        private bool _initialized;
+
+        public ReactiveCommand<Unit, Unit> SaveCommand { get; }
 
         public CodeEditorViewModel(string filePath)
         {
             _filePath = filePath;
+            SaveCommand = ReactiveCommand.CreateFromTask(SaveAsync, this.WhenAnyValue(v => v.IsDirty));
         }
 
         public CodeEditorViewModel() : this("Program.cs") { }
@@ -28,6 +55,7 @@ namespace GameEngine.Editor.ViewModels
         {
             try
             {
+                IsBusy = true;
                 if (File.Exists(_filePath))
                 {
                     var text = await File.ReadAllTextAsync(_filePath);
@@ -37,10 +65,32 @@ namespace GameEngine.Editor.ViewModels
                 {
                     SourceCode = $"// File not found: {_filePath}";
                 }
+                IsDirty = false;
+                _initialized = true;
             }
             catch (Exception ex)
             {
                 SourceCode = $"// Error loading file: {ex.Message}";
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task SaveAsync()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(_filePath)) return;
+                await File.WriteAllTextAsync(_filePath, SourceCode ?? string.Empty);
+                IsDirty = false;
+                // Hook: trigger scene incremental compile here if applicable.
+            }
+            catch (Exception ex)
+            {
+                // Optionally expose a StatusMessage property.
+                Console.WriteLine("Save failed: " + ex);
             }
         }
     }
